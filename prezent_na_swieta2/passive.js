fe4adb10db5962db6585547e981ca831b9a4960f
malgos208 <60302666+malgos208@users.noreply.github.com>
2020-12-24 16:25:32 +0100

    private

---
+++ prezent_na_swieta2/passive.js
@@ -0,0 +1,224 @@
+
+function check(){
+	var aquestion1 = document.quiz1.question1.value;
+
+	var correct = 0;
+
+
+	if (aquestion1 == "24-12-2020") {
+		correct++;
+
+	
+	var messages = ["TPPP - OPD autor: T.E. s.111"];
+	var score;
+
+	if (correct == 1) {
+		score = 0;
+	}
+
+
+	document.getElementById("after_submit1").style.visibility = "visible";
+
+	document.getElementById("message1").innerHTML = messages[score];
+	
+	
+	}
+	
+}
+
+function check2(){
+	var aquestion2 = document.quiz2.question2.value;
+
+	var correct = 0;
+
+
+	if (aquestion2 == "MasSło") {
+		correct++;
+
+	
+	var pictures = ["img/telefon.png" ];
+	var messages = [""];
+	var score;
+
+	if (correct == 1) {
+		score = 0;
+	}
+
+
+	document.getElementById("after_submit2").style.visibility = "visible";
+
+	document.getElementById("message2").innerHTML = messages[score];
+
+	document.getElementById("picture2").src = pictures[score];
+	}
+	
+}
+
+
+function check3(){
+	var aquestion3 = document.quiz3.question3.value;
+
+	var correct = 0;
+
+
+	if (aquestion3 == "7437642954") {
+		correct++;
+
+	
+	var messages = ["Szukaj pod głośnikiem"];
+	var score;
+
+	if (correct == 1) {
+		score = 0;
+	}
+
+
+	document.getElementById("after_submit3").style.visibility = "visible";
+
+	document.getElementById("message3").innerHTML = messages[score];
+
+	}
+	
+}
+
+function check4(){
+	var aquestion4 = document.quiz4.question4.value;
+
+	var correct = 0;
+
+
+	if (aquestion4 == "śsKmmmmw") {
+		correct++;
+
+	
+
+	var messages = ["Nie możesz ich jeść w ciągu tygodnia"];
+	var score;
+
+	if (correct == 1) {
+		score = 0;
+	}
+
+
+	document.getElementById("after_submit4").style.visibility = "visible";
+
+	document.getElementById("message4").innerHTML = messages[score];
+
+
+	}
+	
+}
+
+function check5(){
+	var aquestion5 = document.quiz5.question5.value;
+
+	var correct = 0;
+
+
+	if (aquestion5 == "yellow submarine") {
+		correct++;
+
+	
+	var pictures = ["img/lego.jpg" ];
+	var messages = ["\"A więc drzewo trzeba zrąbać, a potem jego zwłoki ustawić w domu i przyozdobić, tak?! Ależ to... rozkoszne!\""];
+	var score;
+
+	if (correct == 1) {
+		score = 0;
+	}
+
+
+	document.getElementById("after_submit5").style.visibility = "visible";
+
+	document.getElementById("message5").innerHTML = messages[score];
+	
+	document.getElementById("picture5").src = pictures[score];
+
+	}
+	
+}
+
+function check6(){
+	var aquestion6 = document.quiz6.question6.value;
+
+	var correct = 0;
+
+
+	if (aquestion6 == "LÓD W LECIE") {
+		correct++;
+
+	
+
+	var messages = ["___, żeby mocno żyć,<p>___wciąż przed siebie idź<p>Każdy dzień jest od dzisiaj twój "];
+	var score;
+
+	if (correct == 1) {
+		score = 0;
+	}
+
+
+	document.getElementById("after_submit6").style.visibility = "visible";
+
+	document.getElementById("message6").innerHTML = messages[score];
+
+
+	}
+	
+}
+
+
+function check7(){
+	var aquestion7 = document.quiz7.question7.value;
+
+	var correct = 0;
+
+
+	if (aquestion7 == "inflation") {
+		correct++;
+
+	
+	var pictures = ["img/data.jpg" ];
+	var messages = [""];
+	var score;
+
+	if (correct == 1) {
+		score = 0;
+	}
+
+
+	document.getElementById("after_submit7").style.visibility = "visible";
+
+	document.getElementById("message7").innerHTML = messages[score];
+
+	document.getElementById("picture7").src = pictures[score];
+	}
+	
+}
+
+function check8(){
+	var aquestion8 = document.quiz8.question8.value;
+
+	var correct = 0;
+
+
+	if (aquestion8 == "Dzień Dziecka") {
+		correct++;
+
+	
+	var pictures = ["img/pod_stolem.jpg"];
+	var messages = [""];
+	var score;
+
+	if (correct == 1) {
+		score = 0;
+	}
+
+
+	document.getElementById("after_submit8").style.visibility = "visible";
+
+	document.getElementById("message8").innerHTML = messages[score];
+
+	document.getElementById("picture8").src = pictures[score];
+	}
+	
+}
